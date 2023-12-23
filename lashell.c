21fb46152e761c9e665e05600cfb93b221700505
Hugo Latendresse <hugolatendresse@gmail.com>
2023-12-23 00:37:31 -0500

    built in commands todo

--- lashell.c
+++ lashell.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 
 #define MAX_COMMAND_LENGTH 1024
 #define DELIMITER " "
@@ -12,19 +17,75 @@ void execute_command(char* command) {
 }
 
 int main() {
-    char command[MAX_COMMAND_LENGTH];
+  char *line;
+  char **args;
+  int status;
 
-    while (1) {
-        printf("> ");
-        if (!fgets(command, MAX_COMMAND_LENGTH, stdin)) {
-            break;
-        }
+  do {
+    printf("> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
 
-        // Remove trailing newline
-        command[strcspn(command, "\n")] = 0;
+    free(line);
+    free(args);
+  } while (status);
 
-        execute_command(command);
+  return EXIT_SUCCESS;
+}
+
+
+// Function prototypes for built-in commands
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+// List of built-in commands, followed by their corresponding functions
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
+};
+
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+// Implementations of built-in functions
+int lsh_cd(char **args) {
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
     }
+  }
+  return 1;
+}
 
-    return 0;
+int lsh_help(char **args) {
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (int i = 0; i < lsh_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int lsh_exit(char **args) {
+  return 0;
+}
+
+// main function
+int main() {
+  // main loop that reads input and executes commands
 }
