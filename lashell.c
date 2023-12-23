604cae80c6bafb65a79f2fd9f7103066d2fb751f
Hugo Latendresse <hugolatendresse@gmail.com>
2023-12-23 00:59:57 -0500

    essential functions

--- lashell.c
+++ lashell.c
@@ -1,9 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #define MAX_COMMAND_LENGTH 1024
 #define DELIMITER " "
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
+
+char *lsh_read_line(void)
+{
+    char *line = NULL;
+    ssize_t bufsize = 0; // getline will allocate a buffer for us
+
+    if (getline(&line, &bufsize, stdin) == -1){
+        if (feof(stdin)) {
+            // End of file, exit
+            exit(EXIT_SUCCESS);
+        } else  {
+            // Some error occured
+            perror("lsh: getline\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return line;
+}
+
+
+char **lsh_split_line(char *line)
+{
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+
+
+int lsh_launch(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        // This is the child process
+        if (execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        // Error forking
+        perror("lsh");
+    } else {
+        // This is the parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
 
 int lsh_cd(char **args);
 int lsh_help(char **args);
@@ -24,7 +108,6 @@ int lsh_num_builtins()
     return sizeof(builtin_str) / sizeof(char *);
 }
 
-// Implementations of built-in functions
 int lsh_cd(char **args)
 {
     if (args[1] == NULL)
@@ -60,29 +143,25 @@ int lsh_exit(char **args)
     return 0;
 }
 
-
-void execute_command(char *command)
+int lsh_execute(char **args)
 {
-    int lsh_execute(char **args)
+    int i;
+
+    if (args[0] == NULL)
     {
-        int i;
-
-        if (args[0] == NULL)
-        {
-            // An empty command was entered.
-            return 1;
-        }
-
-        for (i = 0; i < lsh_num_builtins(); i++)
-        {
-            if (strcmp(args[0], builtin_str[i]) == 0)
-            {
-                return (*builtin_func[i])(args);
-            }
-        }
-
-        return lsh_launch(args);
+        // An empty command was entered.
+        return 1;
     }
+
+    for (i = 0; i < lsh_num_builtins(); i++)
+    {
+        if (strcmp(args[0], builtin_str[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
 }
 
 int main()
@@ -104,4 +183,3 @@ int main()
 
     return EXIT_SUCCESS;
 }
-
